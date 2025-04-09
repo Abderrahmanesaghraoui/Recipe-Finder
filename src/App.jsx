e3a5fe2d9c162bf0fb99e09cf5697af504c95d72
Abderrahmanesaghraoui <abderrahmanesaghraoui@gmail.com>
2025-04-09 16:16:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import RecipeDetails from './pages/RecipeDetails';
+import Favorites from './pages/Favorites';
+import { Box } from '@mui/material';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +35,19 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        <Router>
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Header />
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recipe/:id" element={<RecipeDetails />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </Box>
+        <Footer />
+      </Box>
+    </Router>
     </>
   )
 }
